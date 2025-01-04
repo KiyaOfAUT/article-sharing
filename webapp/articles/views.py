241668cc15ec15ffd495164b1a125df6810b14e1
KiyaOfAUT <kiarashesmailbeigi@gmail.com>
2025-01-04 17:04:11 +0330

    fix: bug in scheduled task

--- webapp/articles/views.py
+++ webapp/articles/views.py
@@ -16,17 +16,18 @@ def process_tier1_ratings():
     keys = cache.keys('tier1_ratings_batch_*')
     for key in keys:
         article_id = key.split('_')[-1]
-        data = cache.get(key, {"create": [], "update": []})
+        ratings = cache.get(key, [])
+        ratings = list(map(int, ratings))
         cache.delete(key)
 
-        create_ratings = list(map(int, data["create"]))
-        update_ratings = list(map(int, data["update"]))
+        # Directly update sum and count
+        total = sum(ratings)
+        count = len(ratings)
 
         article = Article.objects.filter(pk=article_id).first()
         if article:
-            article.sum_of_rating += sum(create_ratings)
-            article.count_of_rates += len(create_ratings)
-            article.sum_of_rating += sum(update_ratings)
+            article.sum_of_rating += total
+            article.count_of_rates += count
             article.save()
 
 
@@ -35,41 +36,25 @@ def process_tier2_ratings():
     keys = cache.keys('tier2_ratings_batch_*')
     for key in keys:
         article_id = key.split('_')[-1]
-        data = cache.get(key, {"create": [], "update": []})
+        ratings = cache.get(key, [])
+        ratings = list(map(int, ratings))
         cache.delete(key)
 
-        create_ratings = list(map(int, data["create"]))
-        update_ratings = list(map(int, data["update"]))
+        # smooth batch average with statistics
+        batch_total = sum(ratings)
+        batch_count = len(ratings)
+        batch_avg = batch_total / batch_count if batch_count else 0
+        batch_variance = np.var(ratings)
 
         article = Article.objects.filter(pk=article_id).first()
         if article:
             current_avg = article.sum_of_rating / article.count_of_rates if article.count_of_rates else 0
+            weight = 1 / (1 + abs(batch_avg - current_avg) + batch_variance)
+            weighted_total = batch_total * weight
 
-            # Handle newly created ratings
-            if create_ratings:
-                batch_total = sum(create_ratings)
-                batch_count = len(create_ratings)
-                batch_avg = batch_total / batch_count
-                batch_variance = np.var(create_ratings)
-                weight = 1 / (1 + abs(batch_avg - current_avg) + batch_variance)
-                weighted_total = batch_total * weight
-
-                article.sum_of_rating += weighted_total
-                article.count_of_rates += batch_count
-                article.save()
-                current_avg = article.sum_of_rating / article.count_of_rates
-
-            # Handle updated ratings (no count increment)
-            if update_ratings:
-                batch_total = sum(update_ratings)
-                batch_count = len(update_ratings)
-                batch_avg = batch_total / batch_count
-                batch_variance = np.var(update_ratings)
-                weight = 1 / (1 + abs(batch_avg - current_avg) + batch_variance)
-                weighted_total = batch_total * weight
-
-                article.sum_of_rating += weighted_total
-                article.save()
+            article.sum_of_rating += weighted_total
+            article.count_of_rates += batch_count
+            article.save()
 
 
 @shared_task
@@ -77,51 +62,31 @@ def process_tier3_ratings():
     keys = cache.keys('tier3_ratings_batch_*')
     for key in keys:
         article_id = key.split('_')[-1]
-        data = cache.get(key, {"create": [], "update": []})
+        ratings = cache.get(key, [])
+        ratings = list(map(int, ratings))
         cache.delete(key)
-
-        create_ratings = list(map(int, data["create"]))
-        update_ratings = list(map(int, data["update"]))
+        # smooth batch average with statistics
+        batch_total = sum(ratings)
+        batch_count = len(ratings)
+        batch_avg = batch_total / batch_count if batch_count else 0
+        batch_variance = np.var(ratings)
 
         article = Article.objects.filter(pk=article_id).first()
         if article:
             current_avg = article.sum_of_rating / article.count_of_rates if article.count_of_rates else 0
 
-            # Handle newly created ratings (Bayesian + smoothing)
-            if create_ratings:
-                batch_total = sum(create_ratings)
-                batch_count = len(create_ratings)
-                batch_avg = batch_total / batch_count
-                batch_variance = np.var(create_ratings)
-                prior_weight = 500
-                weight = 1 / (1 + abs(batch_avg - current_avg) + batch_variance)
+            #Bayesian smoothing
+            prior_weight = 500  # Adjust as needed
+            weight = 1 / (1 + abs(batch_avg - current_avg) + batch_variance)
 
-                smoothed_total = (
-                    (current_avg * prior_weight + batch_avg * batch_count * weight)
-                    / (prior_weight + batch_count * weight)
-                ) * batch_count * weight
+            smoothed_total = (
+                (current_avg * prior_weight + batch_avg * batch_count * weight)
+                / (prior_weight + batch_count * weight)
+            ) * batch_count * weight
 
-                article.sum_of_rating += smoothed_total
-                article.count_of_rates += batch_count
-                article.save()
-                current_avg = article.sum_of_rating / article.count_of_rates
-
-            # Handle updated ratings (no rate count increment)
-            if update_ratings:
-                batch_total = sum(update_ratings)
-                batch_count = len(update_ratings)
-                batch_avg = batch_total / batch_count
-                batch_variance = np.var(update_ratings)
-                prior_weight = 500
-                weight = 1 / (1 + abs(batch_avg - current_avg) + batch_variance)
-
-                smoothed_total = (
-                    (current_avg * prior_weight + batch_avg * batch_count * weight)
-                    / (prior_weight + batch_count * weight)
-                ) * batch_count * weight
-
-                article.sum_of_rating += smoothed_total
-                article.save()
+            article.sum_of_rating += smoothed_total
+            article.count_of_rates += batch_count
+            article.save()
 
 
         
@@ -203,33 +168,23 @@ class RateArticleView(generics.GenericAPIView):
             if current_rating:
                 article_object.sum_of_rating -= current_rating
                 article_object.sum_of_rating += rating
-            else:
-                article_object.sum_of_rating += rating
-                article_object.count_of_rates += 1
+            article_object.sum_of_rating += rating
+            article_object.count_of_rates += 1
             article_object.save()
         elif 100 <= article["number_of_user_rates"] < 1000:
             cache_key = f'tier1_ratings_batch_{pk}'
-            existing_ratings = cache.get(cache_key, {"create":[], "update":[]})
-            if current_rating:
-                existing_ratings["update"].append(rating-current_rating)
-            else:
-                existing_ratings["create"].append(rating)
+            existing_ratings = cache.get(cache_key, [])
+            existing_ratings.append(rating)
             cache.set(cache_key, existing_ratings, 60*5)
         elif 1000 <= article["number_of_user_rates"] < 10000:
             cache_key = f'tier2_ratings_batch_{pk}'
-            existing_ratings = cache.get(cache_key, {"create":[], "update":[]})
-            if current_rating:
-                existing_ratings["update"].append(rating-current_rating)
-            else:
-                existing_ratings["create"].append(rating)
+            existing_ratings = cache.get(cache_key, [])
+            existing_ratings.append(rating)
             cache.set(cache_key, existing_ratings, 60*10)
         else:
             cache_key = f'tier3_ratings_batch_{pk}'
-            existing_ratings = cache.get(cache_key, {"create":[], "update":[]})
-            if current_rating:
-                existing_ratings["update"].append(rating-current_rating)
-            else:
-                existing_ratings["create"].append(rating)
+            existing_ratings = cache.get(cache_key, [])
+            existing_ratings.append(rating)
             cache.set(cache_key, existing_ratings, 60*20)
 
         return Response({"message": "Rating submitted successfully"}, status=status.HTTP_200_OK)
